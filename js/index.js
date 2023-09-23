e7a3355d912fd6b79ccc36bd7cbf8826883b69d0
TEAMFALCONREAL <131518562+TEAMFALCONREAL@users.noreply.github.com>
2023-09-23 19:55:18 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Navbar 
+let menu= document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+// Navbar
